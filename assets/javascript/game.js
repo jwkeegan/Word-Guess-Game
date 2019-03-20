4635ad5bdd4f1ef3be63a73d55672cdf4511ad59
Joe Keegan <jwkeeg@comcast.net>
2019-03-19 21:43:15 -0500

    js finalized and css updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
 // Let window load
 window.onload = function () {
 
+    // load theme audio
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/media/theme.mp3");
+
+    // listen for play and pause of theme song
+    document.getElementById("theme-button").addEventListener("click", function() {
+        audioElement.play();
+    }),
+
+    document.getElementById("pause-button").addEventListener("click", function() {
+        audioElement.pause();
+    }),
+
     // When the user presses the button...
     document.getElementById("start-button").addEventListener("click", function () {
 
@@ -10,20 +23,46 @@ window.onload = function () {
         var current = document.getElementById("current-word");
         var shadowWord = document.getElementById("shadow-word");
         var remGuesses = document.getElementById("guesses-remaining");
+        var remLine = document.getElementById("remaining-line");
         var guessed = document.getElementById("already-guessed");
+        var guessedLine = document.getElementById("letters-line");
+
 
         // Array of words for game to choose from
-        var words = ["one", "two", "three", "four"];
+        var words = ["michael", "dwight", "jim", "pam", "ryan", "andy", "jan", 
+         "roy", "stanley", "kevin", "meredith", "angela", "oscar", "phyllis", 
+         "kelly", "toby", "creed", "darryl", "erin", "gabe", "holly"];
 
         // initialize dynamic variables
         var curWord;
         var userWord;
         var remaining;
         var lettersGuessed;
-
-        // set wins and losses to 0
         var wins = 0;
         var losses = 0;
+
+        // Create function to print word to guess with blanks
+        function printWord(word) {
+            
+            // create string to be returned later
+            var str = "";
+
+            // check that word isn't empty
+            if (word.length > 0) {
+
+                // add first letter to str
+                str = str + word[0];
+
+                // for the rest of the word, add spaces between each letter and add
+                for (var i = 1; i < word.length; i++) {
+                    str = str + " " + word[i];
+                }
+                
+            }
+
+            // return str
+            return str;
+        }
 
         // Create function to reset game
         function gameReset() {
@@ -46,30 +85,42 @@ window.onload = function () {
             lettersGuessed = [];
 
             // Display instructions
-            instrText.textContent = "Press a Key to Guess that Letter";
+            instrText.textContent = "Guess the Character's Name!";
 
             // Display record
             record.textContent = "Wins-Losses: " + wins + "-" + losses;
 
             // Display "Current Word" and shadow of word
             current.textContent = "Current Word:";
-            shadowWord.textContent = userWord;
+            shadowWord.textContent = printWord(userWord);
 
             // Display Guesses remaining
-            remGuesses.textContent = "Guesses Remaining\n" + remaining;
+            remGuesses.textContent = "Guesses Remaining:";
+            remLine.textContent = remaining;
 
             // Display Letters already guessed
-            guessed.textContent = "Letters Guessed\n" + lettersGuessed;
+            guessed.textContent = "Letters Guessed:";
+            guessedLine.textContent = printWord(lettersGuessed);
 
         }
 
         gameReset();
+
+        // Set array of keys to listen for
+        var validLetters = ["a", "b", "c", "d", "e", "f", "g", 
+        "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+        "s", "t", "u", "v", "x", "y", "z"];
 
         // Listen for Keystrokes
         document.onkeyup = function (event) {
 
             // Declare var to hold user key
             var userGuess = event.key.toLowerCase();
+
+            // Make sure key is a letter
+            if (!validLetters.includes(userGuess)) {
+                return;
+            }
 
             // If the key pressed is correct...
             if (curWord.includes(userGuess)) {
@@ -82,7 +133,7 @@ window.onload = function () {
                 }
 
                 // update DOM
-                shadowWord.textContent = userWord;
+                shadowWord.textContent = printWord(userWord);
             }
 
             // Otherwise, if the key has not already been guessed...
@@ -95,8 +146,10 @@ window.onload = function () {
                 lettersGuessed.push(userGuess);
 
                 // update DOM
-                remGuesses.textContent = "Guesses Remaining\n" + remaining;
-                guessed.textContent = "Letters Guessed\n" + lettersGuessed;
+                remGuesses.textContent = "Guesses Remaining:";
+                remLine.textContent = remaining;
+                guessed.textContent = "Letters Guessed:";
+                guessedLine.textContent = printWord(lettersGuessed);
             }
 
             // Check for game-ending situations
